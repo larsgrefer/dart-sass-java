20d2e2b62bdfc58429b0687a5f8230c089069b72
Lars Grefer <github@larsgrefer.de>
2021-05-02 04:57:58 +0200

    Instal socat on github actions

--- sass-embedded-host/src/test/java/de/larsgrefer/sass/embedded/connection/SocketConnectionIT.java
+++ sass-embedded-host/src/test/java/de/larsgrefer/sass/embedded/connection/SocketConnectionIT.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,18 +18,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @EnabledOnOs(value = {OS.LINUX, OS.MAC})
 class SocketConnectionIT {
 
+    private int port = 0;
+
     private Process socat;
 
     @BeforeEach
-    void setUp() throws IOException {
+    void setUp() throws IOException, InterruptedException {
+
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            port = serverSocket.getLocalPort();
+        }
+
         socat = new ProcessBuilder()
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .redirectOutput(ProcessBuilder.Redirect.INHERIT)
                 .command(
                         "socat",
-                        "tcp-listen:50506,reuseaddr",
+                        "tcp-listen:" + port + ",reuseaddr",
                         "exec:" + ConnectionFactory.getBundledDartExec().getAbsolutePath()
                 ).start();
+
+        Thread.sleep(10);
     }
 
     @AfterEach
@@ -40,7 +50,7 @@ class SocketConnectionIT {
     public void compile() throws IOException, SassCompilationFailedException {
         assertThat(socat.isAlive()).isTrue();
 
-        SocketConnection socketConnection = new SocketConnection(new Socket("localhost", 50506));
+        SocketConnection socketConnection = new SocketConnection(new Socket("localhost", port));
 
         try (SassCompiler compiler = new SassCompiler(socketConnection)) {
             String s = compiler.compileString("body {size: 1+1}");

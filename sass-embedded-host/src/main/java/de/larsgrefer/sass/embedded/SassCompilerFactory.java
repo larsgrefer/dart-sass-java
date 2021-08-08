e89da56e71510013290bb7df93ec4bef257d0015
Lars Grefer <github@larsgrefer.de>
2021-08-08 03:09:26 +0200

    cleanup

--- sass-embedded-host/src/main/java/de/larsgrefer/sass/embedded/SassCompilerFactory.java
+++ sass-embedded-host/src/main/java/de/larsgrefer/sass/embedded/SassCompilerFactory.java
@@ -1,12 +1,14 @@
 package de.larsgrefer.sass.embedded;
 
 import de.larsgrefer.sass.embedded.connection.ConnectionFactory;
+import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
 
 /**
  * @author Lars Grefer
  */
+@UtilityClass
 public class SassCompilerFactory {
 
     public static SassCompiler bundled() throws IOException {

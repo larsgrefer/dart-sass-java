e80faf7d0532e9fc72b8e81a3452ac66f00080e3
Lars Grefer <github@larsgrefer.de>
2022-09-24 02:12:17 +0200

    javadoc

--- sass-embedded-host/src/main/java/de/larsgrefer/sass/embedded/connection/CompilerConnection.java
+++ sass-embedded-host/src/main/java/de/larsgrefer/sass/embedded/connection/CompilerConnection.java
@@ -7,14 +7,28 @@ import java.io.Closeable;
 import java.io.IOException;
 
 /**
+ * Abstract representation of a connection to an embedded sass compiler.
+ *
  * @author Lars Grefer
  * @see ProcessConnection
  * @see ConnectionFactory#bundled()
  */
 public interface CompilerConnection extends Closeable {
 
+    /**
+     * Send the given {@link InboundMessage} to the compiler.
+     *
+     * @param inboundMessage The {@link InboundMessage} to send.
+     * @throws IOException If the communication with the compiler fails.
+     */
     void sendMessage(InboundMessage inboundMessage) throws IOException;
 
+    /**
+     * Read a {@link OutboundMessage} from the compiler.
+     *
+     * @return The next {@link OutboundMessage} sent by the compiler.
+     * @throws IOException If the communication with the compiler fails.
+     */
     OutboundMessage readResponse() throws IOException;
 
 }
